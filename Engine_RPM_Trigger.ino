e8fafc573b1d88d7bf8860a85d06b6b38b16a8a6
Trevor792221 <87502824+Trevor792221@users.noreply.github.com>
2023-07-07 08:43:23 -0500

    Add files via upload

---
+++ Engine_RPM_Trigger.ino
@@ -0,0 +1,43 @@
+int highTime;    //integer for storing high time
+int lowTime;     //integer for storing low time
+int TargetRPM;   //integer for target RPM
+int Cylinders;   //integer for number of cylinders
+int cyl;         //integer for Cylinders / 2
+float period;    //integer for storing period
+float freq;      //storing frequency
+float RPM;       //storing RPM
+
+void setup() {
+  Serial.begin(9600); //starts serial output with 9600 baud rate
+  pinMode(4,INPUT);   //setting pin as input - this is your tach signal
+  pinMode(13,OUTPUT); //setting pin as output
+}
+
+void loop() {
+    TargetRPM=4000;            //sets target RPM
+    Cylinders=6;               //number of cylinders
+    cyl=Cylinders/2;           //Cylinders / 2
+    highTime=pulseIn(4,HIGH);  //read high time
+    lowTime=pulseIn(4,LOW);    //read low time
+    period = highTime+lowTime; //period = highTime + lowTime
+    freq=1000000/period;       //getting frequency with period is in Micro seconds
+    RPM = (freq/cyl)*60; //divide freq by number of cylinders in engine x 0.5
+
+  if (RPM > TargetRPM) {
+    digitalWrite(13, LOW);
+  }                            //sets 13 high if RPM > TargetRPM
+  else {
+    digitalWrite(13, HIGH);
+  }                            //sets 13 low if RPM < TargetRPM
+
+    Serial.println("Target RPM: ");           //prints TargetRPM
+    Serial.println(TargetRPM);
+
+    Serial.println("Tach input signal: ");    //prints freq
+    Serial.println(freq);
+
+    Serial.println("Calculated RPM: ");       //prints RPM
+    Serial.println(RPM);
+
+    delay(100);                //100millisecond delay
+}
---
+++ README.txt
@@ -0,0 +1,29 @@
+Thanks to feynman137 (https://forum.arduino.cc/u/feynman137)
+and Blackfin (https://forum.arduino.cc/u/Blackfin)
+for having figured out how to get a tach signal into the arduino and calculating the RPM
+
+You will need:
+1x 1k Ohm Resistor
+2x 330 Ohm Resistor
+1x 1N4007 Diode
+1x 5.1V Zener Diode
+1x H11L1M Optocoupler
+1x Schematic
+1x Relay Module
+(https://forum.arduino.cc/t/reading-an-engines-rpm-with-arduino/689851/5)
+
+Schematic built here
+(https://forum.arduino.cc/t/reading-an-engines-rpm-with-arduino/689851/36)
+
+Put a 330 Ohm Resistor between pins 4 and 6 similar to the schematic shown here
+(https://forum.arduino.cc/t/reading-an-engines-rpm-with-arduino/689851/49)
+
+On the relay board connect VCC to +5v, GND to -, and IN to pin 13 on the Arduino
+
+If  you need help with the relay board go here (https://randomnerdtutorials.com/guide-for-relay-module-with-arduino/)
+
+Change the TargetRPM and Cylinders for your engine and the RPM you want to trigger the relay at
+
+Have fun
+
+
